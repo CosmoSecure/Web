5d5d525ec77f6c57ae290b12445f4f50946dc9e9
akash2061 <lucifermorningstar2061@gmail.com>
2025-11-19 14:58:29 +0530

    Refactor: Remove AnimatedElement components for static content in various pages

--- components/aurora.tsx
+++ components/aurora.tsx
@@ -2,9 +2,9 @@
 
 export function Aurora() {
     return (
-        <div className="aurora-container mb-80">
-            <div className="aurora aurora-1 mb-80"></div>
-            <div className="aurora aurora-2 mb-40"></div>
+        <div className="aurora-container">
+            <div className="aurora aurora-1"></div>
+            <div className="aurora aurora-2"></div>
             <div className="aurora aurora-3"></div>
         </div>
     )
